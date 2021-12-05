6f726e426598fb7ec3416274ed498f02f67da776
kamkonphrom <kamkonphrom@gmail.com>
2021-12-05 20:44:06 +0700

    Create user schema model and connect database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://kamkon007:kamkon007@cluster0.wourw.mongodb.net/user_maneger?retryWrites=true&w=majority",
+  (err, con) => {
+    if (err) return console.log(err);
+
+    return console.log("Database Connected");
+  }
+);
 
 app.use("/", (req, res) => {
   return res.json({ data: "hello" });
