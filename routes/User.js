502c7fa9b1d4ac979bfa7a2ad0e44974de3ecfb7
kamkonphrom <kamkonphrom@gmail.com>
2021-12-07 21:19:26 +0700

    hash password save to database and check requst body

--- routes/User.js
+++ routes/User.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
 
 const User = require("../models/userModel");
 
-router.post("/register", (req, res) => {
-    console.log(req.body);
-    const result = new User(req.body);
+router.post(
+    "/register",
+    body("username").not().isEmpty(),
+    body("email").isEmail(),
+    body("password")
+    .isLength({ min: 5 })
+    .withMessage("must be at least 5 chars long"),
+    (req, res) => {
+        const errors = validationResult(req);
 
-    result.save((err, user) => {
-        if (err) return res.status(400).json(err);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-        user.password = undefined;
-        return res.status(201).json(user);
-    });
-    // return res.json({ message: "User created" });
-});
+        console.log(req.body);
+
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) return res.status(401).json({ err });
+
+            const result = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hash,
+            });
+
+            result.save((err, user) => {
+                if (err) return res.status(400).json(err);
+
+                user.password = undefined;
+                return res.status(201).json(user);
+            });
+            // return res.json({ message: "User created" });
+        });
+    }
+);
 
 module.exports = router;
